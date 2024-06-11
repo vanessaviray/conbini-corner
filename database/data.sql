0077debb7bd0616d7555f2e9ba5c050fd52906b7
Vanessa Viray <13virayvanessab@gmail.com>
2024-06-11 22:54:19 +0000

    Initial commit

--- database/data.sql
+++ database/data.sql
@@ -9,3 +9,23 @@
 --      ('Learn to code', false),
 --      ('Build projects', false),
 --      ('Get a job', false);
+
+insert into "users"
+  ("emailAddress", "hashedPassword")
+  values
+    ('person@email.com', 'asdfjkl');
+
+insert into "products"
+  ("category", "subcategory", "name", "price", "description", "defaultImageUrl", "secondaryImageUrl", "featuredProduct")
+  values
+    ('category', 'subcategory', 'name', 100, 'description', 'default image', 'secondary image', false);
+
+insert into "shoppingCarts"
+  ("userId")
+  values
+    (1);
+
+insert into "shoppingCartItems"
+  ("cartId", "productId", "quantity")
+  values
+    (1, 1, 1);
--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,42 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "users" (
+  "userId" serial PRIMARY KEY,
+  "emailAddress" text,
+  "hashedPassword" text,
+  "createdAt" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "products" (
+  "productId" serial PRIMARY KEY,
+  "category" text,
+  "subcategory" text,
+  "name" text,
+  "price" integer,
+  "description" text,
+  "defaultImageUrl" text,
+  "secondaryImageUrl" text,
+  "featuredProduct" boolean
+);
+
+CREATE TABLE "shoppingCarts" (
+  "cartId" serial PRIMARY KEY,
+  "userId" integer,
+  "createdAt" timestamptz NOT NULL DEFAULT (now()),
+  "updatedAt" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "shoppingCartItems" (
+  "cartItemId" serial PRIMARY KEY,
+  "cartId" integer,
+  "productId" integer,
+  "quantity" integer
+);
+
+ALTER TABLE "shoppingCarts" ADD FOREIGN KEY ("userId") REFERENCES "users" ("userId");
+
+ALTER TABLE "shoppingCartItems" ADD FOREIGN KEY ("cartId") REFERENCES "shoppingCarts" ("cartId");
+
+ALTER TABLE "shoppingCartItems" ADD FOREIGN KEY ("productId") REFERENCES "products" ("productId");
